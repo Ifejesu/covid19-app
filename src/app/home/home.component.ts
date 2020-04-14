5fefce92a931433627c51c9440a417be8f6f2f49
Goodness Ifejesu Olajide <ifejesugoodness97@gmail.com>
2020-04-14 14:31:59 +0100

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,31 +2,30 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
-export class AppComponent {
-  title = 'covid19-app';
+export class HomeComponent {
   covidForm = this.fb.group({
     regionName: [null, Validators.required],
     avgAge: [null, Validators.required],
-    avgDailyIncomeUSD: [null, Validators.required],
+    avgDailyIncomeInUSD: [null, Validators.required],
     avgDailyIncomePopulation: [null, Validators.required],
-    periodType: ['days', Validators.required],
-    timetoElapse: [null, Validators.required],
+    periodType: [null, Validators.required],
+    timeToElapse: [null, Validators.required],
     reportedCases: [null, Validators.required],
     population: [null, Validators.required],
     totalHospitalBeds: [null, Validators.required]
   });
 
-  period = [
+  periods = [
     { name: 'Days', value: 'days' },
     { name: 'Weeks', value: 'weeks' },
     { name: 'Months', value: 'months' }
   ];
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   onSubmit() {
     alert('Thanks!');
